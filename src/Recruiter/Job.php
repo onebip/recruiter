39099ec894dd25251c464c626f12be3e9679f152
gabriele.lana <gabriele.lana@cleancode.it>
2014-05-08 15:07:32 +0200

    Job can be loaded and executed

--- src/Recruiter/Job.php
+++ src/Recruiter/Job.php
@@ -16,15 +16,25 @@ class Job
 
     public static function around(Workable $toDo, Recruiter $recruiter)
     {
-        $baseDocumentForJob = [
-            'active' => true,
-            'done' => false,
-            'created_at' => new MongoDate(),
-            'attempts' => 0,
-            'locked' => false,
-            'tags' => []
-        ];
-        return new self($baseDocumentForJob, $toDo, $recruiter);
+        return new self(self::initialize(), $toDo, $recruiter);
+    }
+
+    public static function import($document, Recruiter $recruiter)
+    {
+        if (!array_key_exists('workable_class', $document)) {
+            throw new Exception('Unable to import Job without a class');
+        }
+        if (!class_exists($document['workable_class'])) {
+            throw new Exception('Unable to import Job with unknown Workable class');
+        }
+        if (!method_exists($document['workable_class'], 'import')) {
+            throw new Exception('Unable to import Workable without method import');
+        }
+        return new self(
+            $document,
+            $document['workable_class']::import($document['workable_parameters'], $recruiter),
+            $recruiter
+        );
     }
 
     public function __construct($status, Workable $toDo, Recruiter $recruiter)
@@ -46,7 +56,7 @@ class Job
         if ($this->isSheduledLater()) {
             return $this->recruiter->schedule($this);
         }
-        $this->toDo->execute();
+        return $this->executeNow();
     }
 
     public function export()
@@ -73,21 +83,69 @@ class Job
             ($this->instantiatedAt->usec <= $this->status['scheduled_at']->usec);
     }
 
-    public static function import($document, $recruiter)
+    private function executeNow()
     {
-        if (!array_key_exists('workable_class', $document)) {
-            throw new Exception('Unable to import Job without a class');
+        $methodToCallOnWorkable = $this->status['workable_method'];
+        if (!method_exists($this->toDo, $methodToCallOnWorkable)) {
+            throw new Exception('Unknown method on workable instance');
         }
-        if (!class_exists($document['workable_class'])) {
-            throw new Exception('Unable to import Job with unknown Workable class');
+        try {
+            $this->beforeExecution();
+            $result = $this->toDo->$methodToCallOnWorkable();
+            $this->afterExecution($result);
+            return $result;
+
+        } catch(\Exception $exception) {
+            $this->afterFailure($exception);
         }
-        if (!method_exists($document['workable_class'], 'import')) {
-            throw new Exception('Unable to import Workable without method import');
-        }
-        return new self(
-            $document,
-            $document['workable_class']::import($document['workable_parameters'], $recruiter),
-            $recruiter
+    }
+
+    private function beforeExecution()
+    {
+        $this->status['attempts'] += 1;
+        $this->status['last_execution'] = [
+            'scheduled_at' => $this->status['scheduled_at'],
+            'started_at' => new MongoDate(),
+            // TODO: informations on worker?
+        ];
+        unset($this->status['scheduled_at']);
+        $this->recruiter->accept($this);
+    }
+
+    private function afterExecution($result)
+    {
+        $this->status['last_execution'] = array_merge(
+            $this->status['last_execution'], [
+                'ended_at' => new MongoDate(),
+                // TODO: result class, message and trace
+            ]
         );
+        $this->archive();
+    }
+
+    private function afterFailure($exception)
+    {
+        // TODO: apply retry policy
+        $this->archive();
+    }
+
+    private function archive()
+    {
+        $this->status['active'] = false;
+        $this->status['done'] = true;
+        $this->recruiter->archive($this);
+    }
+
+    private static function initialize()
+    {
+        return [
+            'active' => true,
+            'done' => false,
+            'created_at' => new MongoDate(),
+            'workable_method' => 'execute',
+            'attempts' => 0,
+            'locked' => false,
+            'tags' => []
+        ];
     }
 }
