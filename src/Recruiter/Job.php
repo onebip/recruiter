1667c556f337e40452b75fa2458407be7e6d568b
gabriele.lana <gabriele.lana@cleancode.it>
2014-05-08 10:25:32 +0200

    Renamed Job:: in Job::0

--- src/Recruiter/Job.php
+++ src/Recruiter/Job.php
@@ -10,8 +10,8 @@ class Job
 {
     private $toDo;
     private $recruiter;
-    private $document;
     private $instantiatedAt;
+    private $status;
 
     public static function around(Workable $toDo, Recruiter $recruiter)
     {
@@ -29,17 +29,17 @@ class Job
         return new self($baseDocumentForJob, $toDo, $recruiter);
     }
 
-    public function __construct($document, Workable $toDo, Recruiter $recruiter)
+    public function __construct($status, Workable $toDo, Recruiter $recruiter)
     {
         $this->toDo = $toDo;
-        $this->document = $document;
+        $this->status = $status;
         $this->recruiter = $recruiter;
         $this->instantiatedAt = new MongoDate();
     }
 
     public function scheduleTo()
     {
-        $this->document['scheduled_at'] = new MongoDate();
+        $this->status['scheduled_at'] = new MongoDate();
         return $this;
     }
 
@@ -54,20 +54,20 @@ class Job
     public function export()
     {
         return array_merge(
-            $this->document,
-            ['attempts' => new MongoInt32($this->document['attempts'])]
+            $this->status,
+            ['attempts' => new MongoInt32($this->status['attempts'])]
         );
     }
 
     public function isActive()
     {
-        return array_key_exists('active', $this->document) && $this->document['active'];
+        return array_key_exists('active', $this->status) && $this->status['active'];
     }
 
     private function isSheduledLater()
     {
-        return array_key_exists('scheduled_at', $this->document) &&
-            ($this->instantiatedAt->sec <= $this->document['scheduled_at']->sec) &&
-            ($this->instantiatedAt->usec <= $this->document['scheduled_at']->usec);
+        return array_key_exists('scheduled_at', $this->status) &&
+            ($this->instantiatedAt->sec <= $this->status['scheduled_at']->sec) &&
+            ($this->instantiatedAt->usec <= $this->status['scheduled_at']->usec);
     }
 }
