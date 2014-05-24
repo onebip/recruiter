be9c230bb877e247d31229166c5c67c02abf9893
gabriele.lana <gabriele.lana@cleancode.it>
2014-05-24 17:29:29 +0200

    Dashboard: a graph more similar to what we want

--- dashboard/js/charts.js
+++ dashboard/js/charts.js
@@ -1,12 +1,63 @@
-var data = [4, 8, 15, 16, 23, 42];
 
-var x = d3.scale.linear()
-  .domain([0, d3.max(data)])
-  .range([0, 420]);
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-d3.select(".chart")
-  .selectAll("div")
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], 0.1);
+
+var y = d3.scale.linear()
+    .range([height, -10]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10, "%");
+
+var svg = d3.select("body")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var data = d3.csv.parse(
+  "letter,frequency\nA,.08167\nB,.01492\nC,.02782\nD,.04253\nE,.12702\nF,.02288\nG,.02015\nH,.06094\nI,.06966\nJ,.00153\nK,.00772\nL,.04025\nM,.02406\nN,.06749\nO,.07507\nP,.01929\nQ,.00095\nR,.05987\nS,.06327\nT,.09056\nU,.02758\nV,.00978\nW,.02360\nX,.00150\nY,.01974\nZ,-.00074\n",
+  type
+);
+
+x.domain(data.map(function(d) { return d.letter; }));
+y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+  .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Frequency");
+
+svg.selectAll(".bar")
     .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return x(d) + "px"; })
-    .text(function(d) { return d; });
+  .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function(d) { return x(d.letter); })
+    .attr("width", x.rangeBand())
+    .attr("y", function(d) { return y(d.frequency); })
+    .attr("height", function(d) { return height - y(d.frequency); });
+
+function type(d) {
+  d.frequency = parseFloat(d.frequency);
+  return d;
+}
