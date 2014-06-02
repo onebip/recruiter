a512d4dc6c76c0034d4f180dc62e1ecd1808c176
gabriele.lana <gabriele.lana@cleancode.it>
2014-06-02 15:51:15 +0200

    Examples in examples directory

---
+++ examples/jobFailedBecauseOfNonRetriableException.php
@@ -0,0 +1,32 @@
+#!/usr/bin/env php
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Recruiter\Recruiter;
+use Recruiter\AlwaysFail;
+use Recruiter\RetryPolicy;
+use Recruiter\Worker;
+
+$db = (new MongoClient())->selectDB('recruiter');
+$db->drop();
+
+$recruiter = new Recruiter($db);
+
+(new AlwaysFail())
+    ->asJobOf($recruiter)
+    ->retryWithPolicy(
+        (new RetryPolicy\RetryManyTimes(5, 1))
+            ->retryOnlyWhenExceptionIs('DomainException')
+    )
+    ->inBackground()
+    ->execute();
+
+$worker = $recruiter->hire();
+while (true) {
+    printf("Try to do my work\n");
+    $assignments = $recruiter->assignJobsToWorkers();
+    if ($assignments === 0) break;
+    $worker->work();
+    usleep(1200 * 1000);
+}
