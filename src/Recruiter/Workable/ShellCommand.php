36fcd5d8a433dc04eb6dc62fa5708259a718afb5
gabriele.lana <gabriele.lana@cleancode.it>
2014-07-22 17:11:09 +0200

    Port of Workable\ShellCommand

---
+++ src/Recruiter/Workable/ShellCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Recruiter\Workable;
+
+use Recruiter\Workable;
+use Recruiter\WorkableBehaviour;
+
+class ShellCommand implements Workable
+{
+    use WorkableBehaviour;
+
+    private $commandLine;
+
+    public static function fromCommandLine($commandLine)
+    {
+        return new self($commandLine);
+    }
+
+    private function __construct($commandLine)
+    {
+        $this->commandLine = $commandLine;
+    }
+
+    public function execute()
+    {
+        exec($this->commandLine, $output, $returnCode);
+        $output = implode(PHP_EOL, $output);
+        if ($returnCode != 0) {
+            throw new RuntimeException("Command execution failed (return code $returnCode). Output: " . $output);
+        }
+        return $output;
+    }
+
+    protected function export()
+    {
+        return ['command' => $this->commandLine];
+    }
+
+    protected static function import($parameters)
+    {
+        return new self($parameters['command']);
+    }
+}
