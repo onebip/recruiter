ff606ab4c77ebfa2da6695d69036489918f8f0fb
Giorgio Sironi <giorgio.sironi@onebip.com>
2016-03-16 15:08:25 +0100

    Using job.failure.last event in example bootstrap

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -1,3 +1,6 @@
 <?php
 
 echo 'BOOTSTRAP!!!' . PHP_EOL;
+$recruiter->getEventDispatcher()->addListener('job.failure.last', function($event) {
+    error_log("Job definitively failed: " . var_export($event->export(), true));
+});
