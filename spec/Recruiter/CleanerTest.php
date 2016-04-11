3700ec9931bd10c8d395ed989659dd5c5ae1703d
Simone Pasquini <simone.pasquini@onebip.com>
2016-04-11 16:08:13 +0200

    Half commit

--- spec/Recruiter/CleanerTest.php
+++ spec/Recruiter/CleanerTest.php
@@ -2,6 +2,8 @@
 
 namespace Recruiter;
 
+use Timeless\Interval;
+
 class CleanerTest extends \PHPUnit_Framework_TestCase
 {
     protected function setUp()
@@ -18,10 +20,26 @@ class CleanerTest extends \PHPUnit_Framework_TestCase
             $this->jobRepository,
             $this->mongoLock
         );
+
+        $this->interval = new Interval('10');
     }
 
     public function testShouldCreateCleaner()
     {
         $this->assertNotNull($this->cleaner);
+    }
+
+    public function testShouldInvokeCallableIfLockIsAlreadyAquired()
+    {
+        $sentinel = false;
+        $otherwise = function ($message) use (&$sentinel) {
+            $sentinel = true;
+        };
+
+        $this->mongoLock->method('wait');
+        $this->mongoLock->method('acquire');
+        $this->cleaner->ensureIsTheOnlyOne($this->interval, $otherwise);
+
+        $this->assertTrue($sentinel);
     }
 }
--- src/Recruiter/Cleaner.php
+++ src/Recruiter/Cleaner.php
@@ -4,9 +4,14 @@ namespace Recruiter;
 
 use Recruiter\Job\Repository;
 use Onebip\Concurrency\MongoLock;
+use Timeless\Interval;
+use Onebip\Concurrency\LockNotAvailableException;
 
 class Cleaner 
 {
+    const WAIT_FACTOR = 6;
+    const POLL_TIME = 5;
+
     public function __construct(
         Repository $jobRepository,
         MongoLock $mongoLock
@@ -14,5 +19,23 @@ class Cleaner
     {
         $this->jobRepository = $jobRepository;
         $this->mongoLock = $mongoLock;
+    }
+
+    public function ensureIsTheOnlyOne(Interval $timeToWaitAtMost, $otherwise)
+    {
+        try {
+            $this->lock->wait(self::POLL_TIME, $timeToWaitAtMost->seconds() * self::WAIT_FACTOR);
+            $this->lock->acquire($this->leaseTimeOfLock($timeToWaitAtMost));
+        } catch(LockNotAvailableException $e) {
+            $otherwise($e->getMessage());
+        }
+    }
+
+    /**
+     * @return integer  seconds
+     */
+    private function leaseTimeOfLock(Interval $maximumBackoff)
+    {
+        return round($maximumBackoff->seconds() * self::LOCK_FACTOR);
     }
 }
