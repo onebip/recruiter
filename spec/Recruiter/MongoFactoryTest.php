fe04388ec99f9fd9486177389a9f7f06730265e1
Simone Pasquini <simone.pasquini@onebip.com>
2016-04-14 12:09:41 +0200

    Extract the creation of default mongo db into a private method

--- spec/Recruiter/MongoFactoryTest.php
+++ spec/Recruiter/MongoFactoryTest.php
@@ -9,27 +9,44 @@ class MongoFactoryTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         $this->mongoFactory = new MongoFactory();
+        $this->dbHost = 'localhost:27017';
+        $this->dbName = 'recruiter';
     }
 
     public function testShouldCreateAMongoDatabaseConnection()
     {
         $this->assertInstanceOf(
             'MongoDB',
-            $this->mongoFactory->getMongoDb(
-                $host = 'localhost:27017',
-                $options = ['connectTimeoutMS' => '1000'],
-                $dbName = 'recruiter'
-        ));
+            $this->creationOfDefaultMongoDb()
+        );
     }
 
     public function testWriteConcernIsMajorityByDefault()
     {
+        $mongoDb = $this->creationOfDefaultMongoDb();
+        $this->assertEquals('majority', $mongoDb->getWriteConcern()['w']);
+    }
+
+    public function testShouldOverwriteTheWriteConcernPassedInTheOptions()
+    {
         $mongoDb = $this->mongoFactory->getMongoDb(
                 $host = 'localhost:27017',
-                $options = ['connectTimeoutMS' => '1000'],
+                $options = [
+                    'connectTimeoutMS' => '1000',
+                    'w' => '0',
+                ],
                 $dbName = 'recruiter'
         );
 
         $this->assertEquals('majority', $mongoDb->getWriteConcern()['w']);
+    }
+
+    private function creationOfDefaultMongoDb()
+    {
+        return $this->mongoFactory->getMongoDb(
+             $host = $this->dbHost,
+             $options = ['connectTimeoutMS' => '1000'],
+             $dbName = $this->dbName
+        );
     }
 }
