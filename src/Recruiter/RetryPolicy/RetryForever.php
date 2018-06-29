a2555687672c42cb3ddc80674323305d054c7590
Davide Bellettini <davide.bellettini@easywelfare.com>
2018-06-29 15:02:20 +0000

    Add RetryForever retry policy

---
+++ src/Recruiter/RetryPolicy/RetryForever.php
@@ -0,0 +1,53 @@
+<?php
+namespace Recruiter\RetryPolicy;
+
+use Recruiter\RetryPolicy;
+use Recruiter\RetryPolicyBehaviour;
+use Recruiter\JobAfterFailure;
+
+use Timeless as T;
+use Timeless\Interval;
+
+final class RetryForever implements RetryPolicy
+{
+    private $timeToWaitBeforeRetry;
+
+    use RetryPolicyBehaviour;
+
+    public function __construct($timeToWaitBeforeRetry)
+    {
+        if (!($timeToWaitBeforeRetry instanceof Interval)) {
+            $timeToWaitBeforeRetry = T\seconds($timeToWaitBeforeRetry);
+        }
+        $this->timeToWaitBeforeRetry = $timeToWaitBeforeRetry;
+    }
+
+    public static function afterSeconds($timeToWaitBeforeRetry = 60)
+    {
+        return new static($timeToWaitBeforeRetry);
+    }
+
+    public function schedule(JobAfterFailure $job)
+    {
+        $job->scheduleIn($this->timeToWaitBeforeRetry);
+    }
+
+    public function export()
+    {
+        return [
+            'seconds_to_wait_before_retry' => $this->timeToWaitBeforeRetry->seconds()
+        ];
+    }
+
+    public static function import($parameters)
+    {
+        return new self(
+            T\seconds($parameters['seconds_to_wait_before_retry'])
+        );
+    }
+
+    public function maximumNumberOfRetries()
+    {
+        return PHP_INT_MAX;
+    }
+}
